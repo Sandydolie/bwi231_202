8394cdafb14dc1d10e2cea5a6eb57bd1fd89b4da
kaiolange <36693678+kaiolange@users.noreply.github.com>
2020-10-27 11:08:52 +0100

    Vorbereitungscode TUT5

---
+++ bwi231_20_2/src/de/hs_lu/o2s/tutorium/tut5/Disko.java
@@ -0,0 +1,97 @@
+package de.hs_lu.o2s.tutorium.tut5;
+
+public class Disko extends Haus {
+
+	private double eintrittspreis; //Der Preis ist, falls nicht anders angegeben 12.50
+	private int tanzflaeche;
+	private int altersbeschraenkung; //Mindestalter ist, falls nicht anders im Konstruktor angegebn 18
+
+
+	public Disko() {
+		
+	}
+	
+	public Disko(double eintrittspreis1) {
+		this.eintrittspreis = this.eintrittspreiskorrekt(eintrittspreis1);
+	}
+	
+	public Disko(double eintrittspreis, int tanzflaeche) {
+		this(eintrittspreis);
+		this.tanzflaeche = tanzflaeche;
+	}
+	
+	public Disko(double eintrittspreis, int tanzflaeche, int altersbeschraenkung) {
+		this(eintrittspreis,tanzflaeche);
+		this.altersbeschraenkung = altersbeschraenkung;
+		this.alterbeschraenkungkorrekt(this.altersbeschraenkung);
+	}
+
+	public Disko(String strasse) {
+		super(strasse);
+	}
+
+	public Disko(String strasse, int etagen) {
+		super(strasse, etagen);
+	}
+
+	public Disko(String strasse, int etagen, int einwohner) {
+		super(strasse, etagen, einwohner);
+	}
+	
+	public String toString() {
+		String s = super.toString();
+		s += " Die Disko ist für Personen ab einem Alter von " + this.getAltersbeschraenkung() + " Jahren zugelassen. "
+				+ "Der Eintrittspreis beträgt " + this.getEintrittspreis() + " und besitzt eine Tanzfläche von "
+				+ this.getTanzflaeche() + " qm².";
+		return s;
+	}
+
+	public double getEintrittspreis() {
+		return eintrittspreis;
+	}
+
+	public void setEintrittspreis(double eintrittspreis1) {
+		this.eintrittspreis = this.eintrittspreiskorrekt(eintrittspreis1);
+	}
+
+	public int getTanzflaeche() {
+		return tanzflaeche;
+	}
+
+	public void setTanzflaeche(int tanzflaeche) {
+		this.tanzflaeche = tanzflaeche;
+	}
+
+	public int getAltersbeschraenkung() {
+		return altersbeschraenkung;
+	}
+
+	public void setAltersbeschraenkung(int altersbeschraenkung) {
+		this.altersbeschraenkung = altersbeschraenkung;
+	}
+	
+	
+	private void alterbeschraenkungkorrekt(int alterbeschraenkung) {
+		
+	}
+	
+	private double eintrittspreiskorrekt(double eintrittspreis) {
+		
+	}
+
+	@Override
+	public double kostenSanierung(int Preis) {
+		return this.getGrundstuecksFlaeche() * Preis * 1.5;
+	}
+	
+	public double berechneUmsatz(double preis) {
+		return this.getEinwohner() * (preis + this.getEintrittspreis());
+	}
+
+	
+
+	
+
+
+
+}
